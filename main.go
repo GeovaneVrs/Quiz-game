8e13462fad41caa685ad5d98839081adc4f10257
RM-1807 <geovanevalerio1807@gmail.com>
2025-02-12 10:47:34 -0300

    Estruturação de leitura de name

--- main.go
+++ main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+type Question struct {
+	Text    string
+	Options []string
+	Answer  int
+}
+
+type GameState struct {
+	Name     string
+	Points   string
+	Question []Question
+}
+
+func (g *GameState) Init() {
+	fmt.Println("Seja Bem vindo(a) ao quiz")
+	fmt.Println(("Escreva seu nome:"))
+	reader := bufio.NewReader(os.Stdin)
+	name, err := reader.ReadString('\n')
+
+	if err != nil {
+		panic("Erro ao ler a string")
+	}
+
+	g.Name = name
+
+	fmt.Printf("Vamos ao jogo %s", g.Name)
+}
 
 func main() {
-	fmt.Println("oi")
+	game := &GameState{}
+	game.Init()
 }
