2dc43c6cc6d609dc8af07cd57f1a01c7a4a48758
RM-1807 <geovanevalerio1807@gmail.com>
2025-02-12 11:01:43 -0300

    Criação de metodo de leitura de arquivo csv

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"bufio"
+	"encoding/csv"
 	"fmt"
 	"os"
+	"strconv"
 )
 
 type Question struct {
@@ -33,7 +35,46 @@ func (g *GameState) Init() {
 	fmt.Printf("Vamos ao jogo %s", g.Name)
 }
 
+func (g *GameState) ProcessCSV() {
+	f, err := os.Open("quizgo.csv")
+	if err != nil {
+		panic("erro ao ler arquivo")
+	}
+
+	defer f.Close()
+
+	reader := csv.NewReader(f)
+	records, err := reader.ReadAll()
+	if err != nil {
+		panic("Error ao ler csv")
+	}
+
+	for index, record := range records {
+		fmt.Println(index, record)
+
+		if index > 0 {
+			question := Question{
+				Text:    record[0],
+				Options: record[1:5],
+				Answer:  ToInt(record[5]),
+			}
+
+			g.Question = append(g.Question, question)
+		}
+	}
+}
+
 func main() {
 	game := &GameState{}
+	go game.ProcessCSV()
 	game.Init()
+}
+
+func ToInt(s string) int {
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		panic(err)
+	}
+
+	return i
 }
